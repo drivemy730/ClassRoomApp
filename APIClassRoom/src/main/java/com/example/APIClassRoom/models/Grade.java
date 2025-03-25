413b357f651f1f0d33954a78c9b4e6b8f048d901
David Vélez Álvarez <144290919+drivemy730@users.noreply.github.com>
2025-03-25 18:29:23 -0500

    Update Grade.java
    Update entity relationships and mappings

--- APIClassRoom/src/main/java/com/example/APIClassRoom/models/Grade.java
+++ APIClassRoom/src/main/java/com/example/APIClassRoom/models/Grade.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 
 import java.util.Date;
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 
 @Entity
@@ -20,10 +21,10 @@ public class Grade
     private Integer gradeId;
 
     @Column( name = "id_student", nullable = false)
-    private Integer studentId;
+    private Integer studentId; //FK
 
-    @Column( name = "id_course", nullable = false)
-    private Integer courseId;
+    @Column( name = "id_subject", nullable = false)
+    private Integer subjectId;
 
     @Column( name = "student_evaluation_result", nullable = false)
     private Float studentEvaluationResult;
@@ -31,16 +32,30 @@ public class Grade
     @Column( name = "exam_date", nullable = false)
     private Date examDate;
 
+    //RELATIONS//
+
+    //relation with Student
+    @ManyToOne
+    @JoinColumn(name="fk_student", referencedColumnName = "id_student")
+    @JsonBackReference
+    Student student;
+
+    //relation with subject
+    @ManyToOne
+    @JoinColumn(name="fk_subject", referencedColumnName = "id_subject")
+    @JsonBackReference
+    Subject subject;
+
 
     //Constructors
 
     public Grade() {
     }
 
-    public Grade(Integer gradeId, Integer studentId, Integer courseId, Float studentEvaluationResult, Date examDate) {
+    public Grade(Integer gradeId, Integer studentId, Integer subjectId, Float studentEvaluationResult, Date examDate) {
         this.gradeId = gradeId;
         this.studentId = studentId;
-        this.courseId = courseId;
+        this.subjectId = subjectId;
         this.studentEvaluationResult = studentEvaluationResult;
         this.examDate = examDate;
     }
@@ -66,12 +81,12 @@ public class Grade
         this.studentId = studentId;
     }
 
-    public Integer getCourseId() {
-        return courseId;
+    public Integer getSubjectId() {
+        return subjectId;
     }
 
-    public void setCourseId(Integer courseId) {
-        this.courseId = courseId;
+    public void setSubjectId(Integer subjectId) {
+        this.subjectId = subjectId;
     }
 
     public Float getStudentEvaluationResult() {
@@ -95,4 +110,3 @@ public class Grade
 
 
 }
-
