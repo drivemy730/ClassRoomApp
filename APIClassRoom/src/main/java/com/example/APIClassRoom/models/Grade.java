ea23b614d9eb6e04dfe5ef11a3897812c900a33c
David Vélez Álvarez <144290919+drivemy730@users.noreply.github.com>
2025-03-25 14:08:51 -0500

    Update Grade.java
    "Added JPA configurations for entity mappings, table definitions, and column specifications"

--- APIClassRoom/src/main/java/com/example/APIClassRoom/models/Grade.java
+++ APIClassRoom/src/main/java/com/example/APIClassRoom/models/Grade.java
@@ -3,6 +3,8 @@ package com.example.APIClassRoom.models;
 import jakarta.persistence.Entity;
 
 import java.util.Date;
+import jakarta.persistence.*;
+
 
 @Entity
 
@@ -10,14 +12,23 @@ public class Grade
 {
 
 
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_grade")
     private Integer gradeId;
 
+    @Column( name = "id_student", nullable = false)
     private Integer studentId;
 
+    @Column( name = "id_course", nullable = false)
     private Integer courseId;
 
-    private Float grade;
+    @Column( name = "student_evaluation_result", nullable = false)
+    private Float studentEvaluationResult;
 
+    @Column( name = "exam_date", nullable = false)
     private Date examDate;
 
 
@@ -26,14 +37,13 @@ public class Grade
     public Grade() {
     }
 
-    public Grade(Integer gradeId, Integer studentId, Integer courseId, Float grade, Date examDate) {
+    public Grade(Integer gradeId, Integer studentId, Integer courseId, Float studentEvaluationResult, Date examDate) {
         this.gradeId = gradeId;
         this.studentId = studentId;
         this.courseId = courseId;
-        this.grade = grade;
+        this.studentEvaluationResult = studentEvaluationResult;
         this.examDate = examDate;
     }
-
 
 
 
@@ -64,12 +74,12 @@ public class Grade
         this.courseId = courseId;
     }
 
-    public Float getGrade() {
-        return grade;
+    public Float getStudentEvaluationResult() {
+        return studentEvaluationResult;
     }
 
-    public void setGrade(Float grade) {
-        this.grade = grade;
+    public void setStudentEvaluationResult(Float studentEvaluationResult) {
+        this.studentEvaluationResult = studentEvaluationResult;
     }
 
     public Date getExamDate() {
@@ -84,5 +94,5 @@ public class Grade
 
 
 
-
 }
+
